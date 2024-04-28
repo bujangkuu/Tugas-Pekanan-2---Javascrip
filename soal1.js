77238b6383b6766a2ca06585d4264e50ead4ea33
Nama Anda <email@g.com>
2024-04-28 19:03:20 +0700

    first commit

--- soal1.js
+++ soal1.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-
+//Tugas-2 aplikasi akar pangkat dua bilangan genap sanbercode
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -9,8 +9,10 @@ rl.question("Masukkan bilangan genap: ", function(input) {
     const x = parseInt(input);
 
     if (x < 0) {
+        //masukkan bilangan genap
         console.log("Tidak bisa input bilangan negatif");
     } else if (x % 2 !== 0) {
+        //jika memasukkan bilangan selain genap maka tidak bisa
         console.log("Tidak bisa input bilangan ganjil");
     } else {
         const result = Math.sqrt(x);
